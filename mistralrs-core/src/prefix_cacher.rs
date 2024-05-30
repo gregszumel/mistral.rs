c6871481883c82113d7ddd4f2970a339e0ccbf83
Greg Szumel <greg.szumel@gmail.com>
2024-05-29 22:06:11 -0400

    wip byte metrics

--- mistralrs-core/src/prefix_cacher.rs
+++ mistralrs-core/src/prefix_cacher.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    ops::Deref,
+    sync::{Arc, Mutex},
+};
 
 use candle_core::{Device, Result, Tensor};
 use radix_trie::{Trie, TrieCommon, TrieKey};
@@ -29,7 +32,10 @@ pub struct PrefixCacheManager {
     caches: Trie<Tokens, Arc<Mutex<LayerCaches>>>,
     xlora_caches: Option<Trie<Tokens, Arc<Mutex<LayerCaches>>>>,
     device: Device,
-    pub n_on_device: usize,
+    pub total_bytes_limit: usize,
+    pub device_bytes_limit: usize,
+    pub total_bytes: usize,
+    pub device_bytes: usize,
     no_prefix_cache: bool,
     eviction_cache_ptrs: Vec<EvictionCacheGroup>,
 }
@@ -47,10 +53,28 @@ impl PrefixCacheManager {
             caches: Trie::new(),
             xlora_caches: if is_xlora { Some(Trie::new()) } else { None },
             device,
-            n_on_device,
+            device_bytes_limit: 2_000_000,
+            total_bytes_limit: 1_000_000,
+            device_bytes: 0,
+            total_bytes: 0,
             no_prefix_cache,
             eviction_cache_ptrs: Vec::new(),
         }
+    }
+
+    /// Get's the number of bytes in a KV cache.
+    fn get_kv_cache_byte_size(cache: &Vec<Option<(Tensor, Tensor)>>) -> usize {
+        cache
+            .iter()
+            .map(|pair| match pair {
+                Some((t1, t2)) => {
+                    let t1_bytes = t1.elem_count() * t1.dtype().size_in_bytes();
+                    let t2_bytes = t2.elem_count() * t2.dtype().size_in_bytes();
+                    t1_bytes + t2_bytes
+                }
+                None => 0,
+            })
+            .sum()
     }
 
     /// This always keeps the cache on the device. If later on, a new seq cannot be allocated due to memory shortage,
@@ -60,10 +84,29 @@ impl PrefixCacheManager {
             return;
         }
         let cache = Arc::new(Mutex::new(seq.cache().clone()));
+
+        // count bytes in kv cache. TODO: Can we cache this per model?
+        let cache_bytes = PrefixCacheManager::get_kv_cache_byte_size(seq.cache());
+
+        self.total_bytes += cache_bytes;
+        let device = seq.cache().deref()[0].as_ref().unwrap().0.device().clone();
+        let bytes_on_device_increment = match &device {
+            Device::Cpu => 0,
+            _non_cpu => cache_bytes,
+        };
+        self.device_bytes += bytes_on_device_increment;
+
         self.caches
             .insert(seq.get_toks().to_vec().into(), cache.clone());
         if seq.is_xlora() {
             let xlora_cache = Arc::new(Mutex::new(seq.xlora_cache().clone()));
+            let xlora_cache_bytes = PrefixCacheManager::get_kv_cache_byte_size(seq.xlora_cache());
+            self.total_bytes += xlora_cache_bytes;
+            self.device_bytes += match &device {
+                Device::Cpu => 0,
+                _else => xlora_cache_bytes,
+            };
+
             self.xlora_caches
                 .as_mut()
                 .unwrap()
@@ -92,23 +135,9 @@ impl PrefixCacheManager {
         if self.no_prefix_cache {
             return Ok(0);
         }
-        let mut n_on_device = 0;
-        for (cache, _) in &self.eviction_cache_ptrs {
-            if !matches!(
-                get_mut_arcmutex!(cache.as_ref())[0]
-                    .as_ref()
-                    .unwrap()
-                    .0
-                    .device(),
-                Device::Cpu
-            ) {
-                n_on_device += 1;
-            }
-        }
-        let mut n_evicted = 0;
         // Intentionally evict the first ones first, as they are the oldest
         for (cache, xlora_cache) in &self.eviction_cache_ptrs {
-            if n_on_device - n_evicted == self.n_on_device {
+            if self.device_bytes <= self.device_bytes_limit {
                 break;
             }
             if !matches!(
@@ -121,15 +150,21 @@ impl PrefixCacheManager {
             ) {
                 let mut cache = get_mut_arcmutex!(cache);
                 let mut xlora_cache = xlora_cache.as_ref().map(|c| get_mut_arcmutex!(c));
+                let cache_size = PrefixCacheManager::get_kv_cache_byte_size(&mut cache);
+                let xlora_cache_size = if let Some(ref valid_cache) = xlora_cache {
+                    PrefixCacheManager::get_kv_cache_byte_size(&valid_cache)
+                } else {
+                    0
+                };
 
                 Self::cache_to(cache.iter_mut(), &Device::Cpu)?;
                 if let Some(ref mut xlora_cache) = xlora_cache {
                     Self::cache_to(xlora_cache.iter_mut(), &Device::Cpu)?;
                 }
-                n_evicted += 1;
+                self.device_bytes -= cache_size + xlora_cache_size
             }
         }
-        Ok(self.caches.len().saturating_sub(self.n_on_device))
+        Ok(self.total_bytes - self.device_bytes)
     }
 
     /// Evict all the caches to CPU.
@@ -156,6 +191,7 @@ impl PrefixCacheManager {
                 }
             }
         }
+        self.device_bytes = 0;
         Ok(self.caches.len())
     }
 
@@ -193,4 +229,10 @@ impl PrefixCacheManager {
             Ok(None)
         }
     }
+
+    // get rid of all the cache
+    pub fn purge_cache() {}
+
+    // trim the cache size back down to below threshold
+    pub fn trim_cache() {}
 }
